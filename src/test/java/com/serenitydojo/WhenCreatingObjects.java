e72f23d2420f364446cae8d1f575a508e3e7d3fd
halynahnatiuk <124817652+halynahnatiuk@users.noreply.github.com>
2023-02-12 21:56:52 +0200

    Finished Exercise 3 by Halyna Hnatiuk

--- src/test/java/com/serenitydojo/WhenCreatingObjects.java
+++ src/test/java/com/serenitydojo/WhenCreatingObjects.java
@@ -4,4 +4,26 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class WhenCreatingObjects {
+    @Test
+    public void creating_a_dog(){
+        String name = "Fido";
+        String favoriteToy = "Bone";
+        int age = 5;
+
+        Dog fido = new Dog("Fido", "Bone", 5);
+
+        System.out.println(fido.getName());
+        System.out.println(fido.getFavoriteToy());
+        System.out.println(fido.getAge());
+        fido.age = 5;
+    }
+
+    @Test
+    public void checking_a_dog() {
+        Dog fido = new Dog("Fido","Bone", 5);
+        Assert.assertEquals(fido.getName(), "Fido");
+        Assert.assertEquals(fido.getFavoriteToy(), "Bone");
+        Assert.assertEquals(fido.getAge(), 5);
+    }
+
 }
